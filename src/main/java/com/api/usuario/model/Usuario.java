28ca2d214d10168be855c25b8e148c74791b47b2
marlo2222 <marlo_henrique@hotmail.com.br>
2019-08-29 20:43:03 -0300

    build4

--- src/main/java/com/api/usuario/model/Usuario.java
+++ src/main/java/com/api/usuario/model/Usuario.java
@@ -28,15 +28,15 @@ public class Usuario {
     private String SegundoNome;
 
     @Column(name = "matricula", nullable = false, length = 6, unique = true)
-    @Max(value = 6, message = "O tamanho da senha é no maximo 6 caracteres")
+    //@Max(value = 6, message = "O tamanho da senha é no maximo 6 caracteres")
     private String matricula;
 
     @Column(name = "email", nullable = false, unique = true)
-    @Email(message = "o email fornecido nao e valido!")
+    //@Email(message = "o email fornecido nao e valido!")
     private String email;
 
     @Column(name = "senha", nullable = false)
-    @Min(value = 4, message = "a senha deve ter no minimo 6 caracteres")
+    //@Min(value = 4, message = "a senha deve ter no minimo 6 caracteres")
     private String senha;
 
     @Column(name = "curso", nullable = false)
